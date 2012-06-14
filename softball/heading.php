da21bbb2fe396015a621ab3b037a29b913e4ddca
utegrad <utegrad@gmail.com>
2012-06-13 23:27:58 -0700

    Work on putting the login link / button / what ever in the header and took it out of the left menu.

--- softball/heading.php
+++ softball/heading.php
@@ -12,7 +12,15 @@ $heading->writeHeadingImg();
 		 </h1>
 		 <p style="text-align: right; margin: 0 auto; padding: 0;">
 		 	Organize your team&#39;s schedule and communications
-		 </p>	 
+		 </p>	
+		 <div id="login" style="float: right; margin: 0; padding:10px; height: 40px; background-color: gray; ">
+		 	<table><tr style="vertical-align: bottom;"><td>
+		 		<form action="login.php">
+		 		<a>Login:</a><input type="text" size=15 maxlength="40" value="Email Address" name="emailAddress"> <input type="submit">
+		 		</form>
+	 		</td></tr>
+		 	</table>
+		 </div> 
 <?php 
 $heading->closeHeadingDivTag();
 ?>
